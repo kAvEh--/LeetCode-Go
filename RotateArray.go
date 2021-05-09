f8e3f2e93daa2390936f03f8e2e2175423aa524e
kaveh <kaveh.fazaeli@zoodfood.com>
2021-05-09 12:30:59 +0430

    Rotate Array Problem Docs

--- RotateArray.go
+++ RotateArray.go
@@ -1,5 +1,8 @@
 package main
 
+/*Rotate
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
+*/
 func Rotate(nums []int, k int) {
 	r := len(nums) - k%len(nums)
 	temp := append(nums[r:], nums[:r]...)
